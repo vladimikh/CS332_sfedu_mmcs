8d85156f6399d392271de590e56dd9c9850f5edf
vladimikh <vladimirmikhaylovrf@gmail.com>
2018-10-03 19:07:03 +0300

    lab3

---
+++ lab3/raster_algorithms/Form1.Designer.cs
@@ -0,0 +1,258 @@
+﻿namespace raster_algorithms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.chooseColorBtn = new System.Windows.Forms.Button();
+            this.chooseImageBtn = new System.Windows.Forms.Button();
+            this.radioPen = new System.Windows.Forms.RadioButton();
+            this.radioFillColor = new System.Windows.Forms.RadioButton();
+            this.radioFillTexture = new System.Windows.Forms.RadioButton();
+            this.fillColorPan = new System.Windows.Forms.Panel();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioSelectBorder = new System.Windows.Forms.RadioButton();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.chooseBorderColorBtn = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.borderColorPan = new System.Windows.Forms.Panel();
+            this.clearButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(16, 15);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(829, 510);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
+            // 
+            // chooseColorBtn
+            // 
+            this.chooseColorBtn.Location = new System.Drawing.Point(7, 59);
+            this.chooseColorBtn.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.chooseColorBtn.Name = "chooseColorBtn";
+            this.chooseColorBtn.Size = new System.Drawing.Size(145, 32);
+            this.chooseColorBtn.TabIndex = 1;
+            this.chooseColorBtn.Text = "Цвет заливки";
+            this.chooseColorBtn.UseVisualStyleBackColor = true;
+            this.chooseColorBtn.Click += new System.EventHandler(this.chooseColorBtn_Click);
+            // 
+            // chooseImageBtn
+            // 
+            this.chooseImageBtn.Location = new System.Drawing.Point(7, 96);
+            this.chooseImageBtn.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.chooseImageBtn.Name = "chooseImageBtn";
+            this.chooseImageBtn.Size = new System.Drawing.Size(145, 32);
+            this.chooseImageBtn.TabIndex = 2;
+            this.chooseImageBtn.Text = "Текстура";
+            this.chooseImageBtn.UseVisualStyleBackColor = true;
+            this.chooseImageBtn.Click += new System.EventHandler(this.chooseImageBtn_Click);
+            // 
+            // radioPen
+            // 
+            this.radioPen.AutoSize = true;
+            this.radioPen.Location = new System.Drawing.Point(8, 23);
+            this.radioPen.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.radioPen.Name = "radioPen";
+            this.radioPen.Size = new System.Drawing.Size(97, 21);
+            this.radioPen.TabIndex = 8;
+            this.radioPen.Text = "Карандаш";
+            this.radioPen.UseVisualStyleBackColor = true;
+            this.radioPen.CheckedChanged += new System.EventHandler(this.radioPen_CheckedChanged);
+            // 
+            // radioFillColor
+            // 
+            this.radioFillColor.AutoSize = true;
+            this.radioFillColor.Location = new System.Drawing.Point(8, 52);
+            this.radioFillColor.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.radioFillColor.Name = "radioFillColor";
+            this.radioFillColor.Size = new System.Drawing.Size(135, 21);
+            this.radioFillColor.TabIndex = 9;
+            this.radioFillColor.Text = "Заливка цветом";
+            this.radioFillColor.UseVisualStyleBackColor = true;
+            // 
+            // radioFillTexture
+            // 
+            this.radioFillTexture.AutoSize = true;
+            this.radioFillTexture.Location = new System.Drawing.Point(8, 81);
+            this.radioFillTexture.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.radioFillTexture.Name = "radioFillTexture";
+            this.radioFillTexture.Size = new System.Drawing.Size(155, 21);
+            this.radioFillTexture.TabIndex = 10;
+            this.radioFillTexture.Text = "Заливка текстурой";
+            this.radioFillTexture.UseVisualStyleBackColor = true;
+            // 
+            // fillColorPan
+            // 
+            this.fillColorPan.BackColor = System.Drawing.Color.Black;
+            this.fillColorPan.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.fillColorPan.Location = new System.Drawing.Point(159, 59);
+            this.fillColorPan.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.fillColorPan.Name = "fillColorPan";
+            this.fillColorPan.Size = new System.Drawing.Size(33, 31);
+            this.fillColorPan.TabIndex = 11;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioSelectBorder);
+            this.groupBox1.Controls.Add(this.radioPen);
+            this.groupBox1.Controls.Add(this.radioFillColor);
+            this.groupBox1.Controls.Add(this.radioFillTexture);
+            this.groupBox1.Location = new System.Drawing.Point(853, 15);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox1.Size = new System.Drawing.Size(205, 145);
+            this.groupBox1.TabIndex = 12;
+            this.groupBox1.TabStop = false;
+            // 
+            // radioSelectBorder
+            // 
+            this.radioSelectBorder.AutoSize = true;
+            this.radioSelectBorder.Location = new System.Drawing.Point(8, 110);
+            this.radioSelectBorder.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.radioSelectBorder.Name = "radioSelectBorder";
+            this.radioSelectBorder.Size = new System.Drawing.Size(163, 21);
+            this.radioSelectBorder.TabIndex = 11;
+            this.radioSelectBorder.TabStop = true;
+            this.radioSelectBorder.Text = "Выделение границы";
+            this.radioSelectBorder.UseVisualStyleBackColor = true;
+            this.radioSelectBorder.CheckedChanged += new System.EventHandler(this.radioSelectBorder_CheckedChanged);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.chooseBorderColorBtn);
+            this.groupBox2.Controls.Add(this.pictureBox2);
+            this.groupBox2.Controls.Add(this.borderColorPan);
+            this.groupBox2.Controls.Add(this.chooseColorBtn);
+            this.groupBox2.Controls.Add(this.chooseImageBtn);
+            this.groupBox2.Controls.Add(this.fillColorPan);
+            this.groupBox2.Location = new System.Drawing.Point(853, 167);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox2.Size = new System.Drawing.Size(205, 145);
+            this.groupBox2.TabIndex = 13;
+            this.groupBox2.TabStop = false;
+            // 
+            // chooseBorderColorBtn
+            // 
+            this.chooseBorderColorBtn.Location = new System.Drawing.Point(7, 22);
+            this.chooseBorderColorBtn.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.chooseBorderColorBtn.Name = "chooseBorderColorBtn";
+            this.chooseBorderColorBtn.Size = new System.Drawing.Size(145, 32);
+            this.chooseBorderColorBtn.TabIndex = 12;
+            this.chooseBorderColorBtn.Text = "Цвет границы";
+            this.chooseBorderColorBtn.UseVisualStyleBackColor = true;
+            this.chooseBorderColorBtn.Click += new System.EventHandler(this.chooseBorderColorBtn_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox2.Location = new System.Drawing.Point(159, 96);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(33, 31);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 13;
+            this.pictureBox2.TabStop = false;
+            // 
+            // borderColorPan
+            // 
+            this.borderColorPan.BackColor = System.Drawing.Color.Black;
+            this.borderColorPan.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.borderColorPan.Location = new System.Drawing.Point(159, 22);
+            this.borderColorPan.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.borderColorPan.Name = "borderColorPan";
+            this.borderColorPan.Size = new System.Drawing.Size(33, 31);
+            this.borderColorPan.TabIndex = 13;
+            // 
+            // clearButton
+            // 
+            this.clearButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.clearButton.Location = new System.Drawing.Point(944, 490);
+            this.clearButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(108, 28);
+            this.clearButton.TabIndex = 16;
+            this.clearButton.Text = "Очистить";
+            this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.Click += new System.EventHandler(this.clearButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1068, 533);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Растровые алгоритмы";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button chooseColorBtn;
+        private System.Windows.Forms.Button chooseImageBtn;
+        private System.Windows.Forms.RadioButton radioPen;
+        private System.Windows.Forms.RadioButton radioFillColor;
+        private System.Windows.Forms.RadioButton radioFillTexture;
+        private System.Windows.Forms.Panel fillColorPan;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button chooseBorderColorBtn;
+        private System.Windows.Forms.Panel borderColorPan;
+        private System.Windows.Forms.Button clearButton;
+        private System.Windows.Forms.RadioButton radioSelectBorder;
+    }
+}
+
---
+++ lab3/raster_algorithms/Form1.cs
@@ -0,0 +1,361 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace raster_algorithms
+{
+    public partial class Form1 : Form
+    {
+        private Graphics g;
+        Point lastPoint = Point.Empty;
+        bool isMouseDown = false;
+        int penThickness = 1;
+        Color borderColor = Color.FromArgb(255, 0, 0, 0);
+        Color fillColor = Color.Green;        
+        Pen borderPen;
+        Pen fillPen;
+        TextureBrush textureBrush;
+        HashSet<Point> filledPoints = new HashSet<Point>();
+        Point mouseCoord;
+        Bitmap bmp;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            pictureBox1.Image = bmp;
+            g = Graphics.FromImage(pictureBox1.Image);
+            g.Clear(Color.White);
+
+            borderColorPan.BackColor = borderColor;
+            fillColorPan.BackColor = fillColor;
+            update_pens();
+
+            radioPen.Checked = true;
+        }
+
+        private void update_pens()
+        {
+            borderPen = new Pen(borderColor, penThickness);
+            fillPen = new Pen(fillColor, 1);
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            lastPoint = e.Location;
+            isMouseDown = true;
+            mouseCoord = e.Location;
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isMouseDown && radioPen.Checked && lastPoint != null)
+            {
+                    g.DrawLine(borderPen, lastPoint, e.Location);
+                    lastPoint = e.Location; 
+                    pictureBox1.Invalidate();    
+            }
+        }
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            isMouseDown = false;
+            lastPoint = Point.Empty;
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            if (radioFillColor.Checked)
+            {
+                MouseEventArgs m = (MouseEventArgs)e;
+                Point p = m.Location;
+                simpleFloodFill(p);
+            }
+            if (radioFillTexture.Checked)
+            {
+                MouseEventArgs m = (MouseEventArgs)e;
+                Point p = m.Location;
+                textureFill(p);
+            }
+            if (radioSelectBorder.Checked)
+            {
+                selectBorder(Color.Red);
+            }
+            pictureBox1.Invalidate();
+
+        }
+
+        private void chooseBorderColorBtn_Click(object sender, EventArgs e)
+        {
+            ColorDialog colorDlg = new ColorDialog();
+            colorDlg.Color = borderColor;
+            if (colorDlg.ShowDialog() == DialogResult.OK)
+            { 
+                borderColor = colorDlg.Color;
+                borderColorPan.BackColor = colorDlg.Color;
+                update_pens();
+            }
+        }
+
+        private void chooseColorBtn_Click(object sender, EventArgs e)
+        {
+            ColorDialog colorDlg = new ColorDialog();
+            colorDlg.Color = fillColor;
+
+            if (colorDlg.ShowDialog() == DialogResult.OK)
+            {
+                fillColor = colorDlg.Color;
+                fillColorPan.BackColor = colorDlg.Color;
+                update_pens();
+            }
+        }
+
+        private void loadFillImage()
+        {
+            OpenFileDialog openDialog = new OpenFileDialog();
+            openDialog.Filter =
+                "Image Files(*.BMP;*.JPG;*.JPEG;*.GIF;*.PNG)|*.BMP;*.JPG;*.GIF;*.PNG|All files (*.*)|*.*";
+            if (openDialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    Image img = Image.FromFile(openDialog.FileName);
+                    pictureBox2.Image = (Image)img.Clone();
+                    textureBrush = new TextureBrush(img);
+                }
+                catch
+                {
+                    DialogResult rezult = MessageBox.Show("Невозможно открыть выбранный файл",
+                        "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void chooseImageBtn_Click(object sender, EventArgs e)
+        {
+            loadFillImage();
+        }
+
+        private Color getColorAt(Point point)
+        {
+            if (pictureBox1.ClientRectangle.Contains(point))
+                return ((Bitmap)pictureBox1.Image).GetPixel(point.X, point.Y);
+            else
+                return Color.Black;
+        }
+
+        // заливка цветом
+        private void simpleFloodFill(Point p)
+        {
+            Color curr = getColorAt(p);
+            Point leftPoint = p;
+            Point rightPoint = p;
+            if (curr != borderColor && curr != fillColor && pictureBox1.ClientRectangle.Contains(p))
+            {
+                while (curr != borderColor && pictureBox1.ClientRectangle.Contains(leftPoint))
+                {
+                    leftPoint.X -= 1;
+                    curr = getColorAt(leftPoint);
+                }
+                leftPoint.X += 1;
+                curr = getColorAt(p);
+                while (curr != borderColor && pictureBox1.ClientRectangle.Contains(rightPoint))
+                {
+                    rightPoint.X += 1;
+                    curr = getColorAt(rightPoint);
+                }
+                rightPoint.X -= 1;
+                g.DrawLine(fillPen, leftPoint, rightPoint);
+
+                for (int i = leftPoint.X; i <= rightPoint.X; ++i)
+                {
+                    Point upPoint = new Point(i, p.Y + 1);
+                    Color upC = getColorAt(upPoint);
+                    if (upC.ToArgb() != borderColor.ToArgb() && upC.ToArgb() != fillColor.ToArgb() && pictureBox1.ClientRectangle.Contains(upPoint))
+                        simpleFloodFill(upPoint);
+                }
+                for (int i = leftPoint.X; i < rightPoint.X; ++i)
+                {
+                    Point downPoint = new Point(i, p.Y - 1);
+                    Color downC = getColorAt(downPoint);
+                    if (downC.ToArgb() != borderColor.ToArgb() && downC.ToArgb() != fillColor.ToArgb() && pictureBox1.ClientRectangle.Contains(downPoint))
+                        simpleFloodFill(downPoint);
+                }
+                return;
+            }
+        }
+
+        private void DrawHorizontalLineTexture(int x1, int x2, int y)
+        {
+            g.FillRectangle(textureBrush, x1, y, Math.Abs(x2 - x1) + 1, 1);
+            for (int i = x1; i <= x2; ++i)
+                filledPoints.Add(new Point(i, y));
+        }
+
+        private void textureFill(Point p)
+        {
+            if (textureBrush == null)
+                loadFillImage();
+            else
+            {
+                filledPoints.Clear();
+                textureFill2(p);
+            }
+        }
+
+        // заливка текстурой
+        private void textureFill2(Point p)
+        {
+            Color curr = getColorAt(p);
+            Point leftPoint = p;
+            Point rightPoint = p;
+            if (!filledPoints.Contains(p) && pictureBox1.ClientRectangle.Contains(p) && curr != borderColor)
+            {
+                while (curr != borderColor && pictureBox1.ClientRectangle.Contains(leftPoint))
+                {
+                    leftPoint.X -= 1;
+                    curr = getColorAt(leftPoint);
+                }
+                leftPoint.X += 1;
+                curr = getColorAt(p);
+                while (curr != borderColor && pictureBox1.ClientRectangle.Contains(rightPoint))
+                {
+                    rightPoint.X += 1;
+                    curr = getColorAt(rightPoint);
+                }
+                rightPoint.X -= 1;
+                DrawHorizontalLineTexture(leftPoint.X, rightPoint.X, leftPoint.Y);
+                for (int i = leftPoint.X; i <= rightPoint.X; ++i)
+                {
+                    Point upPoint = new Point(i, p.Y + 1);
+                    Color upC = getColorAt(upPoint);
+                    if (!filledPoints.Contains(upPoint) && upC.ToArgb() != borderColor.ToArgb() && pictureBox1.ClientRectangle.Contains(upPoint))
+                        textureFill2(upPoint);
+                }
+                for (int i = leftPoint.X; i < rightPoint.X; ++i)
+                {
+                    Point downPoint = new Point(i, p.Y - 1);
+                    Color downC = getColorAt(downPoint);
+                    if (!filledPoints.Contains(downPoint) && downC.ToArgb() != borderColor.ToArgb() && pictureBox1.ClientRectangle.Contains(downPoint))
+                        textureFill2(downPoint);
+                }
+                return;
+            }
+        }
+
+        private void radioSelectBorder_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioSelectBorder.Checked)
+                pictureBox1.Cursor = Cursors.Cross;
+            else
+                pictureBox1.Cursor = Cursors.Default;
+        }
+
+        // найти точку, принадлежащую границе
+        private Point findStartPoint()
+        {
+            int x = mouseCoord.X;
+            int y = mouseCoord.Y;
+
+            Color bgColor = bmp.GetPixel(mouseCoord.X, mouseCoord.Y);
+            Color currColor = bgColor;           
+            while (x < bmp.Width - 2 && currColor.ToArgb() == bgColor.ToArgb())
+            {
+                x++;
+                currColor = bmp.GetPixel(x, y);
+            }
+             
+            return new Point(x, y);
+        }
+
+        // выделить границу
+        private void selectBorder(Color c)
+        {
+            List<Point> pixels = new List<Point>();
+            Point curr = findStartPoint();
+            Point start = curr;
+            pixels.Add(start);
+            Color borderColor = bmp.GetPixel(curr.X, curr.Y);
+
+            Point next = new Point();
+            int currDir = 6;
+            int nextDir = -1;
+            int moveTo = 0;
+            do
+            {
+                moveTo = (currDir - 2 + 8) % 8;
+                int mt = moveTo;
+                do
+                {
+                    next = curr;
+                    switch (moveTo)
+                    {
+                        case 0: next.X++; nextDir = 0; break;
+                        case 1: next.X++; next.Y--; nextDir = 1; break;
+                        case 2: next.Y--; nextDir = 2; break;
+                        case 3: next.X--; next.Y--; nextDir = 3; break;
+                        case 4: next.X--; nextDir = 4; break;
+                        case 5: next.X--; next.Y++; nextDir = 5; break;
+                        case 6: next.Y++; nextDir = 6; break;
+                        case 7: next.X++; next.Y++; nextDir = 7; break;
+                    }
+
+                    if (next == start)
+                        break;
+
+                    if (bmp.GetPixel(next.X, next.Y) == borderColor)
+                    {
+                        pixels.Add(next);
+                        curr = next;
+                        currDir = nextDir;
+                        break;
+                    }
+                    moveTo = (moveTo + 1) % 8;
+                } while (moveTo != mt);
+            } while (next != start);
+
+            foreach (var p in pixels)
+                bmp.SetPixel(p.X, p.Y, c);
+
+            pixels.Sort(compareY);
+            HashSet<Point> pixset = new HashSet<Point>();
+
+            foreach (var p in pixels)
+            {
+                pixset.Add(p);
+            }
+
+
+            Console.WriteLine(pixset.Count);
+            Console.WriteLine(pixels.Count);
+        }
+
+        private int compareY(Point p1, Point p2)
+        {
+            if (p1.Y < p2.Y)
+                return 1;
+            else if (p1.Y == p2.Y)
+                return 0;
+            return -1;
+        }
+
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            g.Clear(Color.White);
+            pictureBox1.Invalidate();
+        }
+
+        private void radioPen_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
